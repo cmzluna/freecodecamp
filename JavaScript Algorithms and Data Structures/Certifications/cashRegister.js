e54e9b5f4dcff2d92020976aaec2e3bf5d2bb542
cmzluna <claudiomazzoli@gmail.com>
2021-05-03 10:04:17 -0300

    initial files for last projects

---
+++ JavaScript Algorithms and Data Structures/Certifications/cashRegister.js
@@ -0,0 +1,35 @@
+
+function checkCashRegister(price, cash, cid) {
+  var change;
+  
+
+  // if cash-in-drawer is less than the change due, or if you cannot return the exact change.
+  return { status: "INSUFFICIENT_FUNDS", change: [] }
+
+
+  
+  Return {status: "CLOSED", change: [...]} 
+  // with cash-in-drawer as the value for the key change
+ //  if it is equal to the change due.
+
+
+  // function should always return an object with a status key and a change key.
+  return change;
+
+}
+
+change due : 0.5
+
+// buscar mayor cambio posible en array 
+
+// si encontrado
+  // restar 0.5 - (mayor cambio posible)
+
+  // si es mayor a 0
+    // repetir operacion -> buscar mayor cambio posible
+  // si es igual a 0
+    // devolver cambio
+
+
+
+checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
